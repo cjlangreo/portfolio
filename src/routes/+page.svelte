6f7871d3c5c2ad56825a184a24643c14ae816c08
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-03-21 14:29:53 +0800

    reinit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+  import {Heading, Sidebar, SidebarItem} from 'flowbite-svelte'
+</script>
+
+<div class="flex h-full">
+  <Sidebar class="bg-red-600">
+    <SidebarItem label="hellO!"/>
+  </Sidebar>
+</div>
